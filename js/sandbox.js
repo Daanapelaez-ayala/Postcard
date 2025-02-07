65f0541d07e3dd36e89f79b08259049c707a3abb
daana <126dpelaezayala@frhsd.com>
2025-02-07 12:02:25 -0500

    Message

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,9 +1,6 @@
 var states = document.getElementById('states');
 states.onchange = function() {
-    console.log('state index: ' + states.selectedIndex);
-    console.log('state name: ' + states.options[states.selectedIndex].text);
-    console.log('img/' + states.value + '@2x.jpg');
-    document.getElementById('postcard').style.backgroundImage = 'url(img/' + states.value + '.jpg)';
+    document.getElementById('postcard').style.backgroundImage = 'url("./img/' + states.value + '@2x.jpg")';
     document.getElementById('greeting').innerHTML = 'Greetings from';
     document.getElementById('state-heading').innerHTML = states.options[states.selectedIndex].text;
 }
