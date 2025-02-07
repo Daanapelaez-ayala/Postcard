d0b1df27da75abf0b8fdb061e1db426ca4c23471
daana <126dpelaezayala@frhsd.com>
2025-02-07 11:51:40 -0500

    Message

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,7 +1,9 @@
-let selection = document.querySelector('select');
-let result = document.querySelector('states')
-selection.addEventListener( 'change', () => {
-    result.innerHTML = selection.options [selection.selectedIndex].value;
-    console.log(selection.selectedIndex);
-})
-value
+var states = document.getElementById('states');
+states.onchange = function() {
+    console.log('state index: ' + states.selectedIndex);
+    console.log('state name: ' + states.options[states.selectedIndex].text);
+    console.log('img/' + states.value + '@2x.jpg');
+    document.getElementById('postcard').style.backgroundImage = 'url(img/' + states.value + '.jpg)';
+    document.getElementById('greeting').innerHTML = 'Greetings from';
+    document.getElementById('state-heading').innerHTML = states.options[states.selectedIndex].text;
+}
